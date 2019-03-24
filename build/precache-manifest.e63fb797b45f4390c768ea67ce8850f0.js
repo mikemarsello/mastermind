61a22d95d61e6b959e9f3b7ed7f57542b88b02c5
Mike Marsello <michaelmarsello@gmail.com>
2019-03-24 14:29:56 -0500

    add build

---
+++ build/precache-manifest.e63fb797b45f4390c768ea67ce8850f0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "965bb6be93d49b9ec0c871ce85382941",
+    "url": "/mikemarsello/mastermind/static/media/question.965bb6be.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mikemarsello/mastermind/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ed1cd4d58a45ab2f5100",
+    "url": "/mikemarsello/mastermind/static/js/runtime~main.d3c7ebc1.js"
+  },
+  {
+    "revision": "05a8686c4e5199a3bdac",
+    "url": "/mikemarsello/mastermind/static/js/main.68ead808.chunk.js"
+  },
+  {
+    "revision": "6c38a224a5b85184decc",
+    "url": "/mikemarsello/mastermind/static/js/2.69272ca3.chunk.js"
+  },
+  {
+    "revision": "05a8686c4e5199a3bdac",
+    "url": "/mikemarsello/mastermind/static/css/main.70d5a0c8.chunk.css"
+  },
+  {
+    "revision": "2ee51519701c5c14a9e05ad6978e28a0",
+    "url": "/mikemarsello/mastermind/index.html"
+  }
+];
